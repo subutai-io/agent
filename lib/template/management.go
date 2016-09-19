f03ec0e07287ebee075f602282fa9e7b3a411cf2
tzununbekov <t.zununbekov@gmail.com>
2016-09-19 17:24:42 +0600

    Management import fix

--- lib/template/management.go
+++ lib/template/management.go
@@ -28,6 +28,7 @@ func MngInit() {
 		{"lxc.network.veth.pair", "management"},
 		{"lxc.network.script.up", config.Agent.AppPrefix + "wrappers/create_ovs_interface"},
 		{"lxc.network.link", ""},
+		{"lxc.mount", config.Agent.LxcPrefix + "management/fstab"},
 		{"lxc.rootfs", config.Agent.LxcPrefix + "management/rootfs"},
 		{"lxc.rootfs.mount", config.Agent.LxcPrefix + "management/rootfs"},
 		// TODO following lines kept for back compatibility with old templates, should be deleted when all templates will be replaced.
