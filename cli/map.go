22893bbfd75a916b09c121b96f7d77e724742cc0
tzununbekov <t.zununbekov@gmail.com>
2017-03-16 17:50:03 +0600

    ssl certs path and cleanup fix, #78

--- cli/map.go
+++ cli/map.go
@@ -76,6 +76,8 @@ func mapRemove(protocol, internal, external string) {
 			"server "+internal, " ", true)
 	} else {
 		os.Remove(config.Agent.DataPrefix + "nginx-includes/" + protocol + "/" + external + ".conf")
+		os.Remove(config.Agent.DataPrefix + "web/ssl/https-" + external + ".key")
+		os.Remove(config.Agent.DataPrefix + "web/ssl/https-" + external + ".crt")
 	}
 	restart()
 }
