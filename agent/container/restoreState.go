52bcd9a73154e0c2d2661ecfd62537c4b4cf9fc2
tzununbekov <t.zununbekov@gmail.com>
2017-08-23 12:56:37 +0600

    Compatibility fix

--- agent/container/restoreState.go
+++ agent/container/restoreState.go
@@ -1,15 +1,29 @@
 package container
 
 import (
+	"os"
 	"time"
 
+	"github.com/subutai-io/agent/config"
 	"github.com/subutai-io/agent/db"
 	"github.com/subutai-io/agent/lib/container"
 	"github.com/subutai-io/agent/log"
 )
 
+// temporary function to provide backward compatibility with old approach
+// Need to remove it in next release
+func compat() {
+	for _, name := range container.Containers() {
+		if _, err := os.Stat(config.Agent.LxcPrefix + name + "/.start"); !os.IsNotExist(err) {
+			container.AddMetadata(name, map[string]string{"state": "RUNNING"})
+		}
+	}
+}
+
 // StateRestore checks container state and starting or stopping containers if required.
 func StateRestore() {
+	compat()
+
 	bolt, err := db.New()
 	log.Check(log.WarnLevel, "Opening database", err)
 	active := bolt.ContainerByKey("state", "RUNNING")
