6cc16cfa6973a49f8485bcccd375d7d2561fb43e
dilshat <dilshat.aliev@gmail.com>
2018-01-18 16:26:09 +0600

    Revert "testing explicitly setting readonly false before starting container"
    This reverts commit d7eee9e5e2664aba5a7cf59a266f89d37ade8395.

--- cli/start.go
+++ cli/start.go
@@ -5,16 +5,12 @@ import (
 
 	"github.com/subutai-io/agent/lib/container"
 	"github.com/subutai-io/agent/log"
-	"github.com/subutai-io/agent/lib/fs"
 )
 
 // LxcStart starts a Subutai container and checks if container state changed to "running" or "starting".
 // If state is not changing for 60 seconds, then the "start" operation is considered to have failed.
 func LxcStart(name string) {
 	if container.IsContainer(name) && container.State(name) == "STOPPED" {
-
-		fs.ReadOnly(name, false)
-
 		startErr := container.Start(name)
 		for i := 0; i < 60 && startErr != nil; i++ {
 			log.Info("Waiting for container start (60 sec)")
