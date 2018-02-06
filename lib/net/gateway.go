6455aa582ed595f60757853796d0abd76f543238
Dilshat Aliev <daliev@optimal-dynamics.com>
2018-02-06 11:39:14 +0600

    #324 agent stops containers only on signal SIGUSR1 fixed probable memory leaks

--- lib/net/gateway.go
+++ lib/net/gateway.go
@@ -23,31 +23,37 @@ func DelIface(iface string) {
 
 // RestoreDefaultConf restores default values in "hosts" and "resolv.conf" inside container
 func RestoreDefaultConf(contName string) {
+
 	filePath := config.Agent.LxcPrefix + contName + "/rootfs/etc/"
+
 	for _, file := range []string{"hosts", "resolv.conf"} {
 
-		_, err := os.Stat(filePath + file)
-		log.Check(log.PanicLevel, "Checking "+file+" file", err)
+		doRestore(filePath, contName, file)
+	}
+}
 
-		openFile, err := os.Open(filePath + file)
-		log.Check(log.PanicLevel, "Opening "+file+" file", err)
-		defer openFile.Close()
+func doRestore(filePath, contName, file string) {
 
-		fileBck, err := os.Create(filePath + file + ".BACKUP")
-		log.Check(log.PanicLevel, "Creating "+file+" backup file", err)
-		defer fileBck.Close()
+	_, err := os.Stat(filePath + file)
+	log.Check(log.PanicLevel, "Checking "+file+" file", err)
 
-		_, err = io.Copy(fileBck, openFile)
-		log.Check(log.FatalLevel, "Copying "+file+" backup", err)
+	openFile, err := os.Open(filePath + file)
+	log.Check(log.PanicLevel, "Opening "+file+" file", err)
+	defer openFile.Close()
 
+	fileBck, err := os.Create(filePath + file + ".BACKUP")
+	log.Check(log.PanicLevel, "Creating "+file+" backup file", err)
+	defer fileBck.Close()
 
-		val := "domain\tintra.lan\nsearch\tintra.lan\nnameserver\t10.10.10.1"
-		if file == "hosts" {
-			val = "127.0.0.1\tlocalhost\n127.0.1.1\t" + contName
-		}
-		if log.Check(log.WarnLevel, "Applying new config", ioutil.WriteFile(filePath+file, []byte(val), 0644)) {
-			rollbackConf(contName)
-		}
+	_, err = io.Copy(fileBck, openFile)
+	log.Check(log.FatalLevel, "Copying "+file+" backup", err)
+
+	val := "domain\tintra.lan\nsearch\tintra.lan\nnameserver\t10.10.10.1"
+	if file == "hosts" {
+		val = "127.0.0.1\tlocalhost\n127.0.1.1\t" + contName
+	}
+	if log.Check(log.WarnLevel, "Applying new config", ioutil.WriteFile(filePath+file, []byte(val), 0644)) {
+		rollbackConf(contName)
 	}
 }
 
