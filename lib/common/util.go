0edba52068f83d0939c7d6db676808332a36683f
Dilshat Aliev <daliev@optimal-dynamics.com>
2018-05-03 11:34:06 +0600

    Added recovery to ssdp functions

--- lib/common/util.go
+++ lib/common/util.go
@@ -2,6 +2,9 @@ package common
 
 import (
 	"strings"
+	"github.com/subutai-io/agent/log"
+	"runtime"
+	"reflect"
 )
 
 func Splitter(s string, splits string) []string {
@@ -15,4 +18,17 @@ func Splitter(s string, splits string) []string {
 	}
 
 	return strings.FieldsFunc(s, splitter)
+}
+
+func RunNRecover(g func()) {
+	defer func() {
+		if x := recover(); x != nil {
+			log.Warn("Recovered from panic in "+getFunctionName(g), x)
+		}
+	}()
+	g()
+}
+
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
